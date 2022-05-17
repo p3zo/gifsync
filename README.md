f3548d2abbb96f220b08cfcf8bc34269fc4b87ba
p3zo <p3zo@users.noreply.github.com>
2022-05-17 05:46:52 +0200

    Stitch imgs + audio to mp4 w ffmpeg

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 Sync GIFs to music.
 
+[Demo](./output.mp4)
+
 Uses the [TempoCNN models](https://essentia.upf.edu/models.html#tempocnn) provided by Essentia.
 
 ### Usage
 
+Install ffmpeg and essentia.
+
     python sync.py \
-        --song_filepath "audio/gypsy.mp3" \
+        --audio_filepath "audio/gypsy.mp3" \
         --gif_filepath "input.gif" \
         --hit_frame_ixs 0 4 \
-        --output_filepath "output.gif" \
+        --output_filepath "output.mp4" \
         --perfect_loop
--- sync.py
+++ sync.py
@@ -1,15 +1,14 @@
 import argparse
+import os
+import subprocess
 
 import essentia.standard as es
-from PIL import GifImagePlugin, Image
-
-# Define loading strategy to retain input alpha channels in all frames of the output GIF
-GifImagePlugin.LOADING_STRATEGY = GifImagePlugin.LoadingStrategy.RGB_ALWAYS
+from PIL import Image
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--song_filepath",
+        "--audio_filepath",
         help="The path to the song file to sync the gif to.",
     )
     parser.add_argument(
@@ -32,11 +31,18 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    if not args.audio_filepath:
+        args.audio_filepath = "audio/gypsy.mp3"
+        args.gif_filepath = "input.gif"
+        args.hit_frame_ixs = [0, 4]
+        args.output_filepath = "output.mp4"
+        args.perfect_loop = True
+
     # Load audio
-    song_filepath = args.song_filepath
+    audio_filepath = args.audio_filepath
 
     # Estimate BPM
-    audio_11khz = es.MonoLoader(filename=song_filepath, sampleRate=11025)()
+    audio_11khz = es.MonoLoader(filename=audio_filepath, sampleRate=11025)()
     global_bpm, local_bpm, local_probs = es.TempoCNN(
         graphFilename="tempocnn/deeptemp-k16-3.pb"
     )(audio_11khz)
@@ -50,70 +56,92 @@ if __name__ == "__main__":
     gif_filepath = args.gif_filepath
     im = Image.open(gif_filepath)
 
-    # Iterate through the frames
-    frames = []
-    print("Input gif frames:")
-    try:
-        while 1:
-            frames.append(im.copy())
-            ix = im.tell()
-            print("  ", ix, im.format, im.size, im.mode, im.info)
-            im.seek(ix + 1)
-    except EOFError:
-        pass
-
-    n_frames = im.n_frames
-
     hit_frame_ixs = [int(i) for i in args.hit_frame_ixs]
-    print(hit_frame_ixs)
 
-    perfect_loop = args.perfect_loop
-    if perfect_loop:
-        hit_frame_ixs.append(n_frames)
+    if args.perfect_loop:
+        hit_frame_ixs.append(im.n_frames)
 
-    # seconds, sync'd with beats @ 124 bpm
+    # Get seconds for beats @ the estimated bpm
     beat_times = [seconds_per_beat * i for i in range(len(hit_frame_ixs))]
 
-    original_duration = frames[0].info["duration"]
+    # Calculate the duration of each output frame to sync the hits to the audio
     durations = []
     for ix, (hit_frame_ix, htime) in enumerate(zip(hit_frame_ixs, beat_times)):
         if ix == 0:
             continue
-        print(f"hit frame ix {hit_frame_ix} at {htime}")
-        # TODO: what duration should the hit frames have?
 
-        # assign an equal duration to all frames in between the hit frames to linearly interpolate the movement
+        # Assign an equal duration to all frames in between the hit frames to linearly interpolate the movement
+        # TODO: what duration should the hit frames have?
         n_frames_btwn = hit_frame_ix - hit_frame_ixs[ix - 1]
         btwn_frame_duration = (htime - beat_times[ix - 1]) / n_frames_btwn * 1000  # ms
-        print(
-            f"  {n_frames_btwn} frames btwn {hit_frame_ixs[ix - 1]} and {hit_frame_ix} w duration {btwn_frame_duration} ms"
-        )
 
         durations.extend([btwn_frame_duration] * (n_frames_btwn + 1))
 
-    # Duration values get rounded to the nearest 10 ms, and 20 seems to be the minimum value
-    # See https://superuser.com/questions/569924/why-is-the-gif-i-created-so-slow/569950#569950
-    #   "use a delay of at least two hundreds of a second in all cases."
-    # TODO: get around these limitations by stitching the output frames to a video format instead of saving as gif
-    assert not any(d < 20 for d in durations)
+    # Create intermediate image & metadata files for ffmpeg in a temporary directory
+    tmpdir = f"tmp_{os.path.splitext(os.path.basename(gif_filepath))[0]}"
+    if not os.path.isdir(tmpdir):
+        os.mkdir(tmpdir)
 
-    output_filepath = args.output_filepath
+    tmp_txt = os.path.join(tmpdir, "input.txt")
+    tmp_mp4 = os.path.join(tmpdir, "tmp.mp4")
 
-    # TODO: fix output transparency channel
-    frames[0].save(
-        output_filepath,
-        save_all=True,
-        append_images=frames[1 : len(durations)],
-        duration=durations,
-        loop=0,
+    with open(tmp_txt, "w") as fh:
+        im = Image.open(gif_filepath)
+        try:
+            while 1:
+                ix = im.tell()
+
+                img_filename = f"{ix}.png"
+                im.save(os.path.join(tmpdir, img_filename), duration=durations[ix])
+
+                fh.write(f"file '{img_filename}'\n")
+                fh.write(f"duration {durations[ix]}ms\n")
+
+                im.seek(ix + 1)
+        except EOFError:
+            pass
+
+    # Stitch the images together into a video
+    # TODO: preserve transparency channels of input PNGs when concatenating
+    check = subprocess.check_call(
+        [
+            "ffmpeg",
+            "-f",
+            "concat",
+            "-i",
+            tmp_txt,
+            "-vsync",
+            "vfr",
+            "-pix_fmt",
+            "yuv420p",
+            "-y",
+            tmp_mp4,
+        ]
     )
 
-    print("Output gif frames:")
-    out_im = Image.open(output_filepath)
-    try:
-        while 1:
-            ix = out_im.tell()
-            print("  ", ix, out_im.format, out_im.size, out_im.mode, out_im.info)
-            out_im.seek(ix + 1)
-    except EOFError:
-        pass
+    if check == 0:
+        # Add audio and loop the images to the length of the audio
+        # TODO: running this script fails here with an encoding error, but copy/pasting into a REPL doesn't. Why?
+        # TODO: output has several extra video loops after audio ends. Video should end with audio.
+        check2 = subprocess.check_call(
+            [
+                "ffmpeg",
+                "-stream_loop",
+                "-1",
+                "-i",
+                tmp_mp4,
+                "-i",
+                audio_filepath,
+                "-shortest",
+                "-map",
+                "0:v:0",
+                "-map",
+                "1:a:0",
+                "-y",
+                args.output_filepath,
+            ]
+        )
+
+        # Clean up temporary files
+        if check2 == 0:
+            subprocess.run(["rm", "-rf", f"{tmpdir}"])
