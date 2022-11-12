5487e173eb55b85c21606857762709eeac03a74e
p3zo <p3zo@users.noreply.github.com>
2022-11-11 16:47:59 -0800

    Move easing functions to a separate module

---
+++ easing.py
@@ -0,0 +1,42 @@
+"""Easing functions from https://github.com/semitable/easing-functions"""
+
+
+class EasingBase:
+    limit = (0, 1)
+
+    def __init__(self, start: float = 0, end: float = 1, duration: float = 1):
+        self.start = start
+        self.end = end
+        self.duration = duration
+
+    def func(self, t: float) -> float:
+        raise NotImplementedError
+
+    def ease(self, alpha: float) -> float:
+        t = self.limit[0] * (1 - alpha) + self.limit[1] * alpha
+        t /= self.duration
+        a = self.func(t)
+        return self.end * a + self.start * (1 - a)
+
+    def __call__(self, alpha: float) -> float:
+        return self.ease(alpha)
+
+
+class LinearInOut(EasingBase):
+    def func(self, t: float) -> float:
+        return t
+
+
+class QuadEaseInOut(EasingBase):
+    def func(self, t: float) -> float:
+        if t < 0.5:
+            return 2 * t * t
+        return (-2 * t * t) + (4 * t) - 1
+
+
+class CubicEaseInOut(EasingBase):
+    def func(self, t: float) -> float:
+        if t < 0.5:
+            return 4 * t * t * t
+        p = 2 * t - 2
+        return 0.5 * p * p * p + 1
